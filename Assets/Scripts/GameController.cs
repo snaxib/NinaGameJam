407c767c5061def0354d51443eb3c4541293bf3b
Repede <temporary22@gmail.com>
2019-02-20 23:08:22 -0600

    Added a game controller to spawn new cans when the player scores. Enabled multiple trash cans to be placed, and they will call the game controller to update score.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Transform can;
+    public int force;
+    public GameObject player;
+    public GameObject trashcan1;
+    public GameObject trashcan2;
+    private int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start(){
+        score = 0;  
+        SetScoreText();
+        Transform startCan = Instantiate(can, transform.position, transform.rotation);
+        startCan.GetComponent<Rigidbody>().AddForce(TowardsPlayer(player)*-force);
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    } 
+
+    void SetScoreText(){
+        scoreText.text = "Score: " + score.ToString();
+    }
+    void IncrementScore(){
+        score += 1;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    Vector3 TowardsPlayer(GameObject player){
+        Vector3 direction = transform.position- player.transform.position;
+        return direction;
+    }
+
+    void SpawnCan(){
+        Transform canClone = Instantiate(can, transform.position, transform.rotation);
+        canClone.GetComponent<Rigidbody>().AddForce(TowardsPlayer(player)*-force);
+    }
+}
---
+++ Assets/Scripts/TrashController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject gameController;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag ("Can")){
+            Debug.Log("Score!");
+            gameController.SendMessage("IncrementScore");
+            //Debug.Log(score);
+            Destroy(other.gameObject);
+            gameController.SendMessage("SpawnCan");
+     }
+    }
+}
