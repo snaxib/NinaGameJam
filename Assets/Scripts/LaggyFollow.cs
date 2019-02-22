cc440a93fee341ea59e567e496985f5bca578f18
Jeff <Jeffarehappy@gmail.com>
2019-02-22 17:04:40 -0600

    Sped up character. Added tutorial animation. Adjusted lighting. Still want to remove the psx effect

---
+++ Assets/Scripts/LaggyFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaggyFollow : MonoBehaviour
+{
+    public float followSpeed;
+    public GameObject followObject;
+    private Vector3 targetPos;
+    private Quaternion targetRot;
+    public Vector3 positionOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = followObject.transform.position;
+        targetRot = followObject.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = Vector3.Lerp(targetPos, followObject.transform.position, Time.deltaTime * followSpeed);
+        targetRot = Quaternion.Lerp(targetRot, followObject.transform.rotation, Time.deltaTime * followSpeed);
+
+        this.transform.position = targetPos + positionOffset;
+        this.transform.rotation = targetRot;
+    }
+}
